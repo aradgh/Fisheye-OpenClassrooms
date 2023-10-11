0bc558e92d35f38a8e8e7cb16a0ccd79f0cd1fb6
aradgh <arad.ghorreshi@gmail.com>
2023-10-11 16:31:33 +0200

    Ajout de commentaires descriptifs sur les fonctions d'initialisation de la page d'accueil

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,16 +1,21 @@
+// Afficher toutes les cartes des photographes sur la page d'accueil
 async function displayData(photographers) {
   const photographersSection = document.querySelector(".photographer_section");
 
   photographers.forEach((photographer) => {
+    // Comprendre d'abord le pattern Factory Method pour comprendre pourquoi il faut d'abord
+    // passer par photographerTemplate() pour ensuite utiliser la fonction sous-jacente getUserCardDOM()
     const photographerModel = photographerTemplate(photographer);
     const userCardDOM = photographerModel.getUserCardDOM();
     photographersSection.appendChild(userCardDOM);
   });
 }
 
+// Initialiser la page d'accueil
 async function init() {
-  // Récupère les datas des photographes
+  // Récupérer les données des photographes
   const { photographers } = await getPhotographers();
+  // Afficher les données
   displayData(photographers);
 }
 
